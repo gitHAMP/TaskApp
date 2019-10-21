6d14f972209cb0464b0cc718111577ce708b74d5
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2019-10-21 16:49:44 -0500

    Write  an Enum Class for Status and Task POJO with the following properties: title, status of type Status

--- TaskApp/src/main/java/com/hampcode/model/entity/Status.java
+++ TaskApp/src/main/java/com/hampcode/model/entity/Status.java
@@ -1,5 +1,21 @@
 package com.hampcode.model.entity;
 
-public enum Status {
+import java.util.stream.Stream;
 
+public enum Status {
+	OPEN("open"), CLOSED("closed"), REOPENED("reopened");
+
+	private String typeOfStatus;
+
+	Status(String typeOfStatus) {
+		this.typeOfStatus = typeOfStatus;
+	}
+
+	public String getTypeOfStatus() {
+		return typeOfStatus;
+	}
+
+	public static Stream<Status> stream() {
+		return Stream.of(Status.values());
+	}
 }
--- TaskApp/src/main/java/com/hampcode/model/entity/Task.java
+++ TaskApp/src/main/java/com/hampcode/model/entity/Task.java
@@ -1,5 +1,73 @@
 package com.hampcode.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String title;
+
+	@Lob
+	@NotEmpty
+	@Type(type = "org.hibernate.type.TextType")
+	private String content;
+
+	// status may not be null
+	@NotNull
+	@Enumerated(value = EnumType.STRING)
+	private Status status;
+
+	public Task() {
+		this.status = Status.OPEN;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 
 }
