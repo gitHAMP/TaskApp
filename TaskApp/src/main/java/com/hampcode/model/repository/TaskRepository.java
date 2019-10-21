8f14ce98be3fac09f0b69142c3919f0beb936e79
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2019-10-21 16:53:03 -0500

    Write a Crud Repository for your Task entity

--- TaskApp/src/main/java/com/hampcode/model/repository/TaskRepository.java
+++ TaskApp/src/main/java/com/hampcode/model/repository/TaskRepository.java
@@ -1,5 +1,24 @@
 package com.hampcode.model.repository;
 
-public interface TaskRepository {
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.hampcode.model.entity.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE tasks SET status='CLOSED' WHERE id = ?", nativeQuery = true)
+	void closeTask(Long id);
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE tasks SET status='REOPENED' WHERE id = ?", nativeQuery = true)
+	void reopenTask(Long id);
 }
